2d6a46fda5770997597c27283bc4d7832d763487
Melvin Kurian <kurianmelvin@gmail.com>
2022-09-20 19:10:07 +0200

    testing commit and push to branch

--- src/components/service/RecipeListApi.tsx
+++ src/components/service/RecipeListApi.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 // recipe search api
+// testing git upload
 
 function SearchResults() {
   const [data, setData] = useState({ hits: [] });
